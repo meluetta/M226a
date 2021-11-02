8f8fa86f61c35121cffc1812ac6c310f7ca6e712
meluetta <74251675+meluetta@users.noreply.github.com>
2021-11-02 08:05:29 +0100

    Add files via upload

---
+++ src/Start.java
@@ -0,0 +1,12 @@
+public class Start {
+    /**
+     * @author Mia Rascher
+     * @version 1.0
+     * @since 1.0
+     * @throws 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
